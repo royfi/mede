1499e2f6af2b8a4004f199f224965c277bb24650
royfi <royfillekes@hotmail.com>
2026-09-01 12:59:57 +0200

    Add files via upload

--- mede.py
+++ mede.py
@@ -35,25 +35,63 @@ medeDict = {
 
 
 def main():
+    selectieMenu()
+    
+def selectieMenu():
+    print('Menu\n')
+    print('1. Alcoholpercentage berekenen.\n')
+    print('2. Verhoudingen van uw fruitmelomels berekenen\n')
+    print('3. Recepten inzien\n')
+    print('4. Recepten toevoegen.\n')
 
-  
-    alcoholPercentage()
-    fruitMelomel() 
-    #recepten()
+    selectie = int(input("Welke functie wilt u gebruiken? 0 sluit het programma"))
+    
+    while selectie != 0:
+        match selectie:
+            case 1: 
+                alcoholPercentage()
+            case 2:
+                fruitMelomel()
+            case 3:
+                recepten()
+            case 4: 
+                receptenBouwer()
+            case _: 
+                selectie = 0     
+        print('Menu\n')
+        print('1. Alcoholpercentage berekenen.\n')
+        print('2. Verhoudingen van uw fruitmelomels berekenen\n')
+        print('3. Recepten inzien\n')
+        print('4. Recepten toevoegen.\n')
+
+        selectie = int(input("Welke functie wilt u gebruiken? 0 sluit het programma"))
+
+def normaliseerDichtheid(dichtheid):
+    if dichtheid > 2:
+        dichtheid /= 1000
+    return dichtheid
 
 def alcoholPercentage():
-    # test toevoegen, dat ik ook 1050 kan invoeren, en niet alleen 1.050 voor juiste hoeveelheid\
     while True:
         try:
-            startDichtheid = float(input('Wat is uw begin dichtheid / gravity?'))
-            eindDichtheid = float(input('Wat is uw eind dichtheid / gravity?'))
+            startDichtheid = normaliseerDichtheid(
+                float(input('Wat is uw begin dichtheid / gravity?'))
+            )
+            eindDichtheid = normaliseerDichtheid(
+                float(input('Wat is uw eind dichtheid / gravity?'))
+            )
+
+            if eindDichtheid > startDichtheid:
+                print('De einddichtheid mag niet hoger zijn dan de begindichtheid.')
+                continue
+
             break
         except ValueError:
             print('Vul bij beide alstublieft een getal in!')
 
     abv = (startDichtheid - eindDichtheid)*131.25 
-    procent = f'{abv:.2f}%'
-    print(f'Als uw begin dichtheid {startDichtheid} is en uw eind dichtheid {eindDichtheid} dan is uw alcohol percentage {procent}')
+    procent = f'{abv:.2f}%'#
+    print(f'Als uw begin dichtheid {startDichtheid:.3f} is en uw eind dichtheid {eindDichtheid:.3f} dan is uw alcohol percentage {procent}')
 
 def fruitMelomel():
     
@@ -66,12 +104,16 @@ def fruitMelomel():
     
     honing = (medeVolume / 100)*45
     
-    print('Van welke smaak uit de volgende lijst zou u het recept in van willen zien?')
-    print(list(medeDict["smaken"].keys()))
-    medeSmaak = input()
+    smaken = [smaak.title() for smaak in medeDict['smaken'].keys()]
+    print(smaken)
+    medeSmaak = input(
+        'Van welke smaak uit de volgende lijst wilt u het recept zien? '
+    ).strip().lower()
+    
+  
 
     while medeSmaak not in medeDict['smaken'].keys():
-        medeSmaak = input(f'Kies een van de smaken uit de lijst{list(medeDict['smaken'].keys)}')
+        medeSmaak = input(f'Kies een van de smaken uit de lijst {smaken}: ').strip().lower()
 
     melomel = medeDict['smaken'][medeSmaak]['smaakKarakter']
     fruit = medeDict['smaken'][medeSmaak]['fruitBasis']
@@ -89,18 +131,21 @@ def fruitMelomel():
     print(f'Voor uw melomel wordt {honing}kg aan honing aangeraden, en {fruitTotaal}kg aan {medeSmaak} voor een totale hoeveelheid van {medeVolume}L aan mede, voor een {melomel}e smaak')
 
 def recepten():
+    #niet echt een recept nu, moet later dit beter formateren, en echte receptenlijst geven. ipv de dictLijst. mss extra dict met recepten?
+    #  of alleen belangrijke keys uit de dict pakken?
     print('Welke smaak uit de volgende lijst zou u het recept van willen inzien?')
     print(list(medeDict["smaken"].keys()))
-    smaak = input()
+    smaak = input().strip().title()
     pprint.pprint(medeDict["smaken"][smaak])
     
-def receptenBouwer():
+def receptenBouwer(): 
     
-    naam = input('Welk naam wilt u het recept geven?')
+    
+    naam = input('Welk naam wilt u het recept geven?').strip().title()
     
     while True:
         try:
-            fruitBasis = float(input('Wat is de fruitbasis?'))
+            fruitBasis = float(input('Hoeveel delen fruit wilt u per 1 deel honing toevoegen?'))
             break
         except ValueError:
             print('Vul alstublieft een getal in!')
@@ -130,13 +175,6 @@ def receptenBouwer():
     }
 
     pprint.pprint(medeDict['smaken'][naam])
-    
 
-
-#main()
-#alcoholPercentage()
-#fruitMelomel()
-#recepten() 
-receptenBouwer() 
-fruitMelomel()
+main()
 
